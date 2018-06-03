b6375b693db9e67a84fe8d189cfb962e2794224b
zhangke <463580310@qq.com>
2018-06-03 20:49:40 +0800

    1

--- ctrl/collect-ctrl.js
+++ ctrl/collect-ctrl.js
@@ -9,12 +9,13 @@ module.exports = {
         if (!util.paramsIsvalid(parseData.bookmark))return ctx.body = util.creatCodeObj(400, '参数错误');
         let id = ctx.session.user.id ? ctx.session.user.id : ctx.cookies.get('SESSION_ID');
         let verify = await mysql.query('SELECT * FROM bookmark where id=?', [id]);
-        console.log(verify[0].bookmark == parseData.bookmark);
-        console.log(verify[0].bookmark , parseData.bookmark);
-        if (verify.length > 0 && verify[0].bookmark == parseData.bookmark) {
-            return ctx.body = util.creatCodeObj(200, '已收录');
+        if (verify.length > 0) {
+            for (let i = 0; i < verify.length; i++) {
+                if (verify[i].bookmark == parseData.bookmark)return ctx.body = util.creatCodeObj(200, '已收录');
+            }
+            return ctx.body = util.creatCodeObj(200, '添加成功');
         } else {
-            mysql.query('INSERT INTO bookmark(id,bookmark,href) VALUES(?,?,?)', [id, parseData.bookmark,parseData.href]);
+            mysql.query('INSERT INTO bookmark(id,bookmark,href) VALUES(?,?,?)', [id, parseData.bookmark, parseData.href]);
             return ctx.body = util.creatCodeObj(200, '添加成功');
         }
     },
